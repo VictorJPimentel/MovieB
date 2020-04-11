052b380153b359f837f28c5c20ddb0ceea312ea1
Victor Pimentel <victorjotapimentel@gmail.com>
2020-04-11 18:34:10 -0400

    My Project MovieB

---
+++ login.php
@@ -0,0 +1,19 @@
+<div class="container-front">
+  <div class="front-text">
+    <h1>Book it then review it</h1>
+    <p>The plataform to plan your next movie adventure. Let us know how that adventure went. This is a ramdom text to perfectly fit the slogan</p>
+ </div>
+  <div class="container-login">
+    <form action="/action_page.php">
+        <div class="col">
+          <span class="register-letter">Email</span>
+          <input type="text" name="email"  placeholder="example@example.eg" required>
+          <span class="register-letter">Username</span>
+          <input type="text" name="username" placeholder="@nickname" required>
+          <span class="register-letter">Password</span>
+          <input type="password" name="password" placeholder="*********" required>
+          <input type="submit" value="Sign Up">
+        </div>   
+    </form>
+  </div>
+</div>
