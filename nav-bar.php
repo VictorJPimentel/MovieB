79b82ca2a0ca6b1f8e9a5052867d8b802d17899b
Victor Pimentel <victorjotapimentel@gmail.com>
2020-04-13 22:15:28 -0400

    First commit

--- nav-bar.php
+++ nav-bar.php
@@ -1,15 +1,41 @@
 <nav class="navbar navbar-expand-lg py-3">
     <div class="container"><a href="#" class="navbar-brand text-uppercase font-weight-bold">MovieB</a>
+      <?php if ( isset($_SESSION['userId']) ) {
+      echo '<span class="navbar-brand text-uppercase font-weight-bold">Welcome '.$_SESSION['userUid'].'</span>'; }?>
+
         <button type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation" class="navbar-toggler"> <span class="navbar-toggler-icon popcorn-icon"></span></button>
         <div id="navbarSupportedContent" class="collapse navbar-collapse">
             <ul class="navbar-nav ml-auto">
-                <li class="nav-item active"><a href="#" class="nav-link text-uppercase font-weight-bold">Home <span class="sr-only">(current)</span></a></li>
+                <li class="nav-item active"><a href="./index.php" class="nav-link text-uppercase font-weight-bold">Home <span class="sr-only">(current)</span></a></li>
                 <li class="nav-item"><a href="#" class="nav-link text-uppercase font-weight-bold">About</a></li>
                 <li class="nav-item"><a href="#" class="nav-link text-uppercase font-weight-bold">HOT NOW</a></li>
                 <li class="nav-item"><a href="#" class="nav-link text-uppercase font-weight-bold">Movies</a></li>
                 <li class="nav-item"><a href="#" class="nav-link text-uppercase font-weight-bold">Contact</a></li>
-                <li class="nav-item"><a href="#" class="nav-link text-uppercase font-weight-bold"><span class=""></span> Login</a></li>
-                <li class="nav-item"><a href="#" class="nav-link text-uppercase font-weight-bold"><span class="sign-list"><span class="navbar-toggler-icon nav-icon user-icon"></span> Sign Up</span></a></li>
+                    <?php
+      if ( !isset($_SESSION['userId']) ) {
+        echo '<form class="form-inline" action="includes/login.inc.php" method="post">
+  <div class="form-group mb-2">
+    <label  class="sr-only">Email</label>
+    <input type="text" readonly class="form-control" name="mailuid" placeholder="Username" required">
+  </div>
+  <div class="form-group mx-sm-3 mb-2">
+    <label  class="sr-only">Password</label>
+    <input type="password" class="form-control" name="pwd" placeholder="Password" required>
+  </div>
+  <input type="submit" class="btn  mb-2" name="login-submit" value="Sign In">
+</form>';
+      }
+    ?>
+
+
+
+                     
+
+                <?php if ( isset($_SESSION['userId']) ) {
+                  echo '<li class="nav-item"><form action="includes/logout.inc.php" method="post">
+                        <span class="sign-list"><span class="navbar-toggler-icon nav-icon user-icon"></span><button  type="submit" name="logout-submit">Logout</button></span>
+                        </form></li>';  }
+                    ?>
             </ul>
         </div>
     </div>
---
+++ signup.php
@@ -0,0 +1,21 @@
+<div class="container-front">
+  <div class="front-text">
+    <h1>Book it then review it</h1>
+    <p>The platform to help you plan your next movie adventure. Let us know how that adventure went. This is a ramdom text to perfectly fit the slogan</p>
+ </div>
+  <div class="container-login">
+    <form action="includes/signup.inc.php" method="post">
+        <div class="col">
+          <span class="register-letter">Email</span>
+          <input class="input-normal" type="text" name="mail"  placeholder="example@example.eg" required>
+          <span class="register-letter">Username</span>
+          <input class="input-normal" type="text" name="uid" placeholder="Username" required>
+          <span class="register-letter">Password</span>
+          <input class="input-normal" type="password" name="pwd" placeholder="Password" required>
+          <span class="register-letter">Password</span>
+          <input class="input-normal" type="password" name="pwd-repeat" placeholder="Repeat Password" required>
+          <input class="input-normal" type="submit" name="signup-submit" value="Sign Up">
+        </div>
+    </form>
+  </div>
+</div>
