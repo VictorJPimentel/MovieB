ac7fd732ca75a3c70b87482ca87ad03897950e13
edwin-26 <j-26@outlook.es>
2020-05-12 16:58:07 -0400

    Few extra movies in the slides

--- temporary.php
+++ temporary.php
@@ -15,7 +15,7 @@
       <img class="d-block w-25" src="https://i.pinimg.com/originals/cf/1e/17/cf1e1715cf973fa7d4959c136550a12d.jpg" alt="Second slide">
     </div>
     <div class="carousel-item">
-      <img class="d-block w-25" src="https://m.media-amazon.com/images/M/MV5BMGU2NzRmZjUtOGUxYS00ZjdjLWEwZWItY2NlM2JhNjkxNTFmXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_.jpg" alt="Third slide">
+      <img class="d-block w-25" src="https://irs.www.warnerbros.com/gallery-v2-jpeg/inception_posterlarge_8-1308772917.jpg" alt="Third slide">
 
 	</div>
 
@@ -24,6 +24,12 @@
     </div>
     <div class="carousel-item">
       <img class="d-block w-25" src="https://m.media-amazon.com/images/M/MV5BNGNhMDIzZTUtNTBlZi00MTRlLWFjM2ItYzViMjE3YzI5MjljXkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_.jpg" alt="Sixth slide">
+    </div>
+	<div class="carousel-item">
+      <img class="d-block w-25" src="https://lh3.googleusercontent.com/zvuqfxWWv581MWVTlstjf66dO868Olsa1GJcMzYoKQs_wt6jnN5BHRbnhdbKG5XK_F0ZuA" alt="Sixth slide">
+    </div>
+	<div class="carousel-item">
+      <img class="d-block w-25" src="https://m.media-amazon.com/images/M/MV5BZTMxMGM5MjItNDJhNy00MWI2LWJlZWMtOWFhMjI5ZTQwMWM3XkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_UY1200_CR80,0,630,1200_AL_.jpg" alt="Sixth slide">
     </div>
   </div>
   <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
