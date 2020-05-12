936257d7b0d336e48e41a559c6f9e5d9d17677a6
sticknanton <nick.a.stanton@gmail.com>
2020-05-12 13:53:18 -0500

    small css adjustments

--- includes/contact.inc.php
+++ includes/contact.inc.php
@@ -13,6 +13,16 @@ if (isset($_POST['contact-submit'])) {
     }else {
       mysqli_stmt_bind_param($stmt, "sss", $userId, $message, $resolved);
       mysqli_stmt_execute($stmt);
+
+      $to_email = 'classictheatreaministrator@gmail.com';
+      $subject = 'Contact Form Submit';
+      $headers = 'From: noreply @ theclassic . com';
+      $retval = mail($to_email,$subject,$message,$headers);
+      if( $retval == true ) {
+            echo "Message sent successfully...";
+         }else {
+            echo "Message could not be sent...";
+         }
       header("Location: ../contact.php?contact=success");
       exit();
   }
